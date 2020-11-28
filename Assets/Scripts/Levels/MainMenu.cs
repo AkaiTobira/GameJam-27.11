c3e9ca8a1f31f862b73798f0fd126728b4e2991c
aliszka <agata.anna.debiec@gmail.com>
2020-11-28 22:30:53 +0100

    Add main and pause menu

---
+++ Assets/Scripts/Levels/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public static MainMenu Instance;
+    public GameObject creditsFrame;
+    public Button start;
+
+    void Awake()
+    {
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        creditsFrame.SetActive(false);
+    }
+    
+    public void StartButton()
+    {
+        SceneManager.LoadSceneAsync("_Game");
+        start.interactable = false;
+    }
+    public void CreditsButton()
+    {
+        creditsFrame.SetActive(true);
+    }
+    public void CreditsBackButton()
+    {
+        creditsFrame.SetActive(false);
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
