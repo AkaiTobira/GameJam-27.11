298e6b7dfad69b88249301c1d66bbfb036ea48c8
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-11-28 12:28:51 +0100

    Add Jump smoothing and reorganising

--- Assets/Scripts/FSM/JumpState.cs
+++ Assets/Scripts/FSM/JumpState.cs
@@ -8,17 +8,24 @@ public class JumpState : BaseState, IState
     bool accelerateJumpforce = true;
     float elapsedTime = 0;
 
+    Rigidbody2D _playerRB;
+
     public JumpState(GameObject gameObject) : base(gameObject){}
     public void OnEnter(){
         PlayerDetector.Instance.Jump();
         accelerateJumpforce = true;
+        _playerRB = PlayerDetector.Instance.GetComponent<Rigidbody2D>();
 
         PlayerAnimator.Instance.SetBool("Jumping", true);
+        PlayerAnimator.Instance.PAnimator.SetBool("OnGround", false);
     }
+    
     public void OnExit()
     {
+        PlayerAnimator.Instance.PAnimator.SetBool("OnGround", true);
         PlayerAnimator.Instance.SetBool("Jumping", false);
     }
+
     public override void HandleInput(){
         PlayerAnimator.Instance.UpdateSide((int)Input.GetAxisRaw("Horizontal"));
         if ( accelerateJumpforce ){
@@ -30,15 +37,16 @@ public class JumpState : BaseState, IState
         }
     }
 
-    public override void ProcessGraphics(){}
+    public override void ProcessGraphics(){
+        PlayerAnimator.Instance.PAnimator.SetFloat("JumpDirection", Mathf.Sign(_playerRB.velocity.y));
+        PlayerAnimator.Instance.SetBool("Moving", PlayerInput.isLeftHold() || PlayerInput.isRightHold());
+        
+    }
     public override void ProcessPhysics(){
-
-
         PlayerDetector.Instance.Move((int)Input.GetAxisRaw("Horizontal"));
         if( PlayerDetector.Instance.isOnGround( ) && !accelerateJumpforce){
             CameraShake.Instance.TriggerShake(0.1f);
             _stateMachine.ChangeToState( new IdleState(_gameObject));
         }
-
     }
 }
