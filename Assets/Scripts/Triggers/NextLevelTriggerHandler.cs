640728a8b52f2d0e702e3b582b98272296657cb6
JacobMiki <JacobMiki@users.noreply.github.com>
2020-11-28 20:02:17 +0100

    Trigger zones

---
+++ Assets/Scripts/Triggers/NextLevelTriggerHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelTriggerHandler : MonoBehaviour, ITriggerHandler
+{
+    public bool HandleTrigger(Collider2D other)
+    {
+        LevelManager.Instance.NextLevel();
+        return true;
+    }
+}
