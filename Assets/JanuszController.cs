0dbfa53a86128dd439f0a6216c7472f8329cd3c9
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-11-29 06:45:54 +0100

    Janusz workInPorgress

---
+++ Assets/JanuszController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JanuszController : MonoBehaviour, IPointerDownHandler
+{
+
+    [SerializeField] private BoxCollider2D JanuszBox;
+    [SerializeField] private Animator JanuszAnimator;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        JanuszAnimator.SetTrigger("Outplayed");
+        JanuszBox.enabled = false;
+    }
+
+}
