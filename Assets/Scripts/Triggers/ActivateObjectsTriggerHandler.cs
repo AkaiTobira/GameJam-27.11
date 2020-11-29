1bc1df604605ed2a6fa710646a8b5b4e659b4b75
JacobMiki <JacobMiki@users.noreply.github.com>
2020-11-29 01:58:45 +0100

    Activate Game Object Trigger and Event Trigger

---
+++ Assets/Scripts/Triggers/ActivateObjectsTriggerHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTriggerHandler : MonoBehaviour, ITriggerHandler
+{
+    [SerializeField] private UnityEvent _onTrigger = new UnityEvent();
+    public bool HandleTrigger(Collider2D other)
+    {
+        _onTrigger.Invoke();
+        return true;
+    }
+}
---
+++ Assets/Scripts/Triggers/EventTriggerHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class ActivateObjectDef
+{
+    public GameObject gameObject;
+    public bool enabled;
+}
+
+public class ActivateObjectsTriggerHandler : MonoBehaviour, ITriggerHandler
+{
+    [SerializeField] private ActivateObjectDef[] _activateObjects;
+    public bool HandleTrigger(Collider2D other)
+    {
+        foreach (var def in _activateObjects)
+        {
+            def.gameObject.SetActive(def.enabled);
+        }
+        return true;
+    }
+}
